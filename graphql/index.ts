73074d858f4fa1043c6bb62b97ebfdd9c72fc7da
M.Sharjeel <muhammadsharjeel819@gmail.com>
2023-07-18 11:39:51 +0500

    Update version

--- graphql/index.ts
+++ graphql/index.ts
@@ -48,7 +48,7 @@ export const createProjectMutation = `
 
 export const projectsQuery = `
   query getProjects($category: String, $endcursor: String) {
-    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category }}) {
       pageInfo {
         hasNextPage
         hasPreviousPage
